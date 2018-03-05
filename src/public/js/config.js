ce9ae95eb140cbe267e7ebf9794d009175da322d
Administrator <790168710@qq.com>
2018-03-05 13:47:11 +0800

    修改打包路径

--- src/public/js/config.js
+++ src/public/js/config.js
@@ -1,5 +1,6 @@
 //export const serveUrl = 'http://www.kjg.com:8080'
-export const serveUrl = 'http://scstm.api.pthola.cn'
+//export const serveUrl = 'http://scstm.api.pthola.cn'
+export const serveUrl = 'http://218.6.173.219:8081/api/public'
 
 export function getAjax(url, params, resolveBack, rejectErr, _this) {
   _this.$http.defaults.headers.common['Authorization'] = 'Bearer ' + sessionStorage.getItem('token'),

7eef5ef59b7b51a0a1b7a915de34a27a97dd0323
sunshine824 <790168710@qq.com>
2018-07-04 17:53:29 +0800

    细节优化

--- src/components/exhibit/theater.vue
+++ src/components/exhibit/theater.vue
@@ -113,7 +113,6 @@
             type: this.type
           },
           (res) => {
-            console.log(res)
             this.theaterData = res.data
           }, (err) => {
             console.log(err)
